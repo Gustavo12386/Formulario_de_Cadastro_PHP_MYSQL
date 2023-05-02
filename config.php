0bdb580d8cde124cb91f18daa1aa3d4816b05d97
Gustavo12386 <gucalderaro@hotmail.com>
2023-05-02 15:43:57 -0300

    primeiro commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbhost = "127.0.0.1";
+$dbuser = "root";
+$dbPassword = "Gusang_20011";
+$dbname = "formulario_php";
+
+$conexao = new mysqli($dbhost, $dbuser, $dbPassword, $dbname);
+
+?>
